db4d163215a166ff6e25b0946a1d614d6d206f84
NP-Sinh <npsinh6103@gmail.com>
2026-03-22 20:02:35 +0700

    update 22/03/26

---
+++ Services/ImageCompressionService.cs
@@ -0,0 +1,10 @@
+﻿namespace MoiEx.Services
+{
+    public interface IImageCompressionService
+    {
+        Task<dynamic> CompressImagesAsync(List<IFormFile> files, int quality = 75, int maxWidth = 1920, int maxHeight = 1920, CancellationToken cancellationToken = default);
+    }
+    public class ImageCompressionService
+    {
+    }
+}
