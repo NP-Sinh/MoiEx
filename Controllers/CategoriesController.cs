d2fe8dd4615382d32662d3858d645e546de438d0
NP-Sinh <npsinh6103@gmail.com>
2026-03-24 10:17:33 +0700

    nén ảnh

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoiEx.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Services/ImageCompressionService.cs
+++ Services/ImageCompressionService.cs
@@ -2,9 +2,10 @@
 {
     public interface IImageCompressionService
     {
-        Task<dynamic> CompressImagesAsync(List<IFormFile> files, int quality = 75, int maxWidth = 1920, int maxHeight = 1920, CancellationToken cancellationToken = default);
+        
     }
-    public class ImageCompressionService
+    public class ImageCompressionService : IImageCompressionService
     {
+       
     }
 }
